692e0eb117f5dd2e52673cc30f82b25686b212f7
Shubham <shubhamshahrising@gmail.com>
2020-12-23 13:22:01 +0530

    :wrench: Allow temp-literals  instead of double

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  extends: `react-app`,
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    globals: {
+        __PATH_PREFIX__: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", 4],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-  },
+    extends: `react-app`,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        semi: ["error", "never"],
+    },
 }
