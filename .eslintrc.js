c804e392303295f6295f47c8a681cf0750522f31
Shubham <shubhamshahrising@gmail.com>
2020-12-24 22:16:47 +0530

    :wrench: Allow tag literals

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,17 @@ module.exports = {
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double", { allowTemplateLiterals: true }],
         semi: ["error", "never"],
-        "import/no-anonymous-default-export": ["error", "never"],
+        "import/no-anonymous-default-export": [
+            "error",
+            {
+                allowArray: true,
+                allowArrowFunction: true,
+                allowAnonymousClass: true,
+                allowAnonymousFunction: true,
+                allowCallExpression: true, // The true value here is for backward compatibility
+                allowLiteral: true,
+                allowObject: true,
+            },
+        ],
     },
 }
