72bc76c93e2d6ae3ef11d959755aa94beea39a4e
Shubham <shubhamshahrising@gmail.com>
2020-12-25 09:30:45 +0530

    :wrench: Use local file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,13 @@ module.exports = {
                 pathToConfigModule: `src/utils/typography`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data/`,
+            },
+        },
+        `gatsby-transformer-csv`,
     ],
 }
