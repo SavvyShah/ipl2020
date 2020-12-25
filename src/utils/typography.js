627917e2d2312c8595fe087f09b371a63559aaab
Shubham <shubhamshahrising@gmail.com>
2020-12-25 13:16:11 +0530

    :bento: Roboto font

--- src/utils/typography.js
+++
@@ -1,9 +0,0 @@
-import Typography from "typography"
-import funstonTheme from "typography-theme-funston"
-const typography = new Typography(funstonTheme)
-
-const { rhythm, scale } = typography
-
-export { rhythm, scale }
-
-export default typography
