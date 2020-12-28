fd008875363c2d21f581ba0add5775a2e98e6125
Shubham <shubhamshahrising@gmail.com>
2020-12-28 14:20:27 +0530

    :sparkles: Become more useful

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -2,12 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./Select.module.css"
 
-export default function Select({ selectRef, options, name }) {
+export default function Select({
+    selectRef,
+    options,
+    name,
+    onChange,
+    className,
+}) {
     return (
-        <select className={styles.select} name={name} ref={selectRef}>
-            <option value="">Countries</option>
-            {options.map((option) => (
-                <option value={option}>{option}</option>
+        <select
+            onChange={onChange}
+            className={`${styles.select} ${className}`}
+            name={name}
+            ref={selectRef}
+        >
+            <option value="">{name}</option>
+            {options.map((option, key) => (
+                <option key={`${option}${key}`} value={option}>{option}</option>
             ))}
         </select>
     )
@@ -17,4 +28,6 @@ Select.propTypes = {
     selectRef: PropTypes.any,
     options: PropTypes.arrayOf(PropTypes.string),
     name: PropTypes.string,
+    onChange: PropTypes.func,
+    className: PropTypes.string,
 }
